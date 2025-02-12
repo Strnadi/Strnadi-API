da70f95296572b426e9c52f9f9bb84e5599a790e
poproshaikin <@poproshaikin>
2025-02-12 20:32:32 +0100

    add cors policy

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -2,6 +2,7 @@ using ApiGateway.Services;
 using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 Env.Load("../.env");
 
@@ -9,7 +10,19 @@ builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddCors(corsOptions =>
+{
+    corsOptions.AddPolicy(configuration["CORS_DEFAULT"], policyBuilder =>
+    {
+        policyBuilder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
