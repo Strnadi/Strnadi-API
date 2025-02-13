57e5f6cb1d8384b917e49413f452c000957064ac
poproshaikin <@poproshaikin>
2025-02-13 11:49:15 +0100

    add cors, routing and remove appsettings.json from index

--- DataAccessGate/Program.cs
+++ DataAccessGate/Program.cs
@@ -1,8 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
+builder.Services.AddCors(corsOptions =>
+{
+    corsOptions.AddPolicy(configuration["CORS:Default"], policyBuilder =>
+    {
+        policyBuilder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 app.MapControllers();
+app.UseRouting();
 app.Run();
