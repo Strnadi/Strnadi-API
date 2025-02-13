cf094ec173c7e94aaf2bef93cd6f78f0ed01a3b3
poproshaikin <stasstrong06@gmail.com>
2025-02-13 17:07:00 +0100

    implementing recordings uploading, catching some exceptions

---
+++ ApiGateway/Services/FileSystemHelper.cs
@@ -0,0 +1,27 @@
+namespace ApiGateway.Services;
+
+public class FileSystemHelper
+{
+    private readonly string _pathToRecordingsDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}/binRecordings/";
+    
+    /// <returns>Path of the generated file</returns>
+    public string SaveRecordingSoundFile(int recordingId, int recordingPartId, byte[] data)
+    {
+        CreateDirectoryIfNotExists(recordingId);
+
+        string path = _pathToRecordingsDirectory + $"{recordingId}_{recordingPartId}.wav";
+        File.WriteAllBytes(path, data);
+
+        return path;
+    }
+
+    private void CreateDirectoryIfNotExists(int recordingId)
+    {
+        string path = _pathToRecordingsDirectory + recordingId;
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
