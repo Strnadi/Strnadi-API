ad3e078015beb562bc525cd3fab580131faead00
poproshaikin <@poproshaikin>
2025-02-13 13:16:48 +0100

    fix db connection

--- DataAccessGate/Sql/RepositoryBase.cs
+++ DataAccessGate/Sql/RepositoryBase.cs
@@ -5,12 +5,14 @@ using Shared.Logging;
 
 namespace DataAccessGate.Sql;
 
-internal abstract class RepositoryBase : IDisposable
+internal abstract class RepositoryBase
 {
     protected DbConnection _connection;
+    protected string _connectionString;
     
     protected RepositoryBase(string connectionString)
     {
+        _connectionString = connectionString;
         Logger.Log(connectionString);
         _connection = new NpgsqlConnection(connectionString);
         _connection.Open();
