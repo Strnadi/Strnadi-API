2d3f8e7f19b73d1c9326dfb27f739118c1eb2891
poproshaikin <@poproshaikin>
2025-02-14 11:45:10 +0100

    fix path creating errors

--- DataAccessGate/Services/FileSystemService.cs
+++ DataAccessGate/Services/FileSystemService.cs
@@ -2,14 +2,14 @@ namespace DataAccessGate.Services;
 
 public class FileSystemHelper
 {
-    private readonly string _pathToRecordingsDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}/binRecordings/";
+    private readonly string _pathToRecordingsDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}binRecordings/";
     
     /// <returns>Path of the generated file</returns>
     public string SaveRecordingSoundFile(int recordingId, int recordingPartId, byte[] data)
     {
         CreateDirectoryIfNotExists(recordingId);
 
-        string path = _pathToRecordingsDirectory + $"{recordingId}_{recordingPartId}.mp3";
+        string path = _pathToRecordingsDirectory + $"{recordingId}/" + $"{recordingId}_{recordingPartId}.mp3";
         File.WriteAllBytes(path, data);
 
         return path;
