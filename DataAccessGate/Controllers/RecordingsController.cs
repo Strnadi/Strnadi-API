fa83fded1d539c265d661fbd69166954f16161a7
poproshaikin <stasstrong06@gmail.com>
2025-02-17 10:54:16 +0100

    now passing explicitly serialized recording model instead of object

--- DataAccessGate/Controllers/RecordingsController.cs
+++ DataAccessGate/Controllers/RecordingsController.cs
@@ -13,6 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
+using System.Text.Json;
 using DataAccessGate.Sql;
 using Microsoft.AspNetCore.Mvc;
 using Models.Database;
@@ -50,7 +52,7 @@ public class RecordingsController : ControllerBase
         }
 
         Logger.Log($"Recording {recording.Id} was sent to download.");
-        return Ok(recording);
+        return Ok(JsonSerializer.Serialize(recording));
     }
     
     [HttpPost("upload")]
