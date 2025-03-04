92f838a9b8e6aa70b7f8318eb64125e0b565e404
poproshaikin <@poproshaikin>
2025-03-04 13:03:31 +0100

    feat: add initial NotificationService implementation with Firebase integration

---
+++ NotificationService/Program.cs
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2024 Stanislav Motsnyi
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+FirebaseApp.Create(new AppOptions
+{
+    Credential = GoogleCredential.FromJson(configuration["Firebase:ServiceAccount"])
+});
+
+builder.Services.AddScoped<NotificationService.Services.NotificationService>();
+
+builder.Services.AddControllers();
+builder.Services.AddCors(corsOptions =>
+{
+    corsOptions.AddPolicy(configuration["CORS:Default"], policyBuilder =>
+    {
+        policyBuilder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+app.Run();
