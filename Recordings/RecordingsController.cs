3c77932e6d11d8cb4b494c0a4a36d726407ca7d4
poproshaikin <stasstrong06@gmail.com>
2025-03-13 09:11:44 +0100

    implement recordings/upload endpoint

--- Recordings/RecordingsController.cs
+++ Recordings/RecordingsController.cs
@@ -1,5 +1,9 @@
+using Auth.Services;
+using Microsoft.AspNetCore.Http;
 using Repository;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+using Shared.Models.Requests;
 
 namespace Recordings;
 
@@ -13,7 +17,43 @@ public class RecordingsController : ControllerBase
         [FromQuery] bool parts = false,
         [FromQuery] bool sound = false)
     {
-        var recordings = await repo.Get(email, parts, sound);
+        var recordings = await repo.GetAsync(email, parts, sound);
         return Ok(recordings);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetRecording(int id,
+        [FromServices] RecordingsRepository repo,
+        [FromQuery] bool parts = false,
+        [FromQuery] bool sound = false)
+    {
+        var recording = await repo.GetAsync(id, parts, sound);
+        
+        if (recording is null)
+            return NotFound("Recording with provided ID not found");
+        
+        return Ok(recording);
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload([FromBody] RecordingUploadModel recording,
+        [FromServices] JwtService jwtService,
+        [FromServices] UsersRepository usersRepo,
+        [FromServices] RecordingsRepository recordingsRepo)
+    {
+        string? jwt = this.GetJwt();
+
+        if (jwt is null)
+            return BadRequest("No JWT provided");
+        
+        if (!jwtService.TryValidateToken(jwt, out string? email))
+            return Unauthorized();
+        
+        var recordingId = await recordingsRepo.UploadAsync(email!, recording);
+        
+        if (recordingId is null)
+            return StatusCode(500, "Failed to upload recording");
+
+        return Ok(recordingId);
     }
 }
