8c9019e565e86149f4fc77386c8e30edf4570baf
poproshaikin <stasstrong06@gmail.com>
2025-03-14 12:24:21 +0100

    fix names

--- Shared/Models/Database/Recordings/RecordingModel.cs
+++ Shared/Models/Database/Recordings/RecordingModel.cs
@@ -20,7 +20,7 @@ public class RecordingModel
 {
     public int Id { get; set; }
     
-    public int UserId { get; set; }
+    public int UserEmail { get; set; }
     
     public DateTime CreatedAt { get; set; }
     
