fad77f0b0cbb5649b524c221443c6730ec88657a
poproshaikin <@poproshaikin>
2025-03-14 11:40:32 +0100

    implement utils/health endpoint

---
+++ Utils/UtilsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Utils;
+
+[ApiController]
+[Route("utils")]
+public class UtilsController : ControllerBase
+{
+    [HttpHead("health")]
+    public IActionResult Health() => Ok();
+}
