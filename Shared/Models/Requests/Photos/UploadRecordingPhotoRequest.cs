c2b28954ff4bc19e325bdd2ae72fbecf91cde1fc
poproshaikin <@poproshaikin>
2025-03-15 22:02:56 +0100

    implement photos/recording-photo POST endpoint

---
+++ Shared/Models/Requests/Photos/UploadRecordingPhotoRequest.cs
@@ -0,0 +1,8 @@
+namespace Shared.Models.Requests.Photos;
+
+public class UploadRecordingPhotoRequest
+{
+    public int RecordingId { get; set; }
+    
+    public string[] PhotosBase64 { get; set; }
+}
