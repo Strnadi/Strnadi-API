ff32700979666a4db20e48717d0996ab4167b73c
Jan Drobílek <drobilek.jan@gmail.com>
2025-04-16 21:39:44 +0200

    Zdenda map fix

--- Utils/MapController.cs
+++ Utils/MapController.cs
@@ -21,13 +21,21 @@ public class MapController : ControllerBase
         var targetUrl = $"https://api.mapy.cz/{path}{query}";
 
         using var client = new HttpClient();
-        
+
         var req = new HttpRequestMessage(HttpMethod.Get, targetUrl);
         req.Headers.Add("X-Mapy-Api-Key", _configuration["MapyCzKey"]);
-        
-        var res = await client.SendAsync(req);
 
-        var content = await res.Content.ReadAsStringAsync();
-        return Content(content, "*/*");
+        var res = await client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead);
+        Response.RegisterForDispose(res);
+
+        if (!res.IsSuccessStatusCode)
+        {
+            return StatusCode((int)res.StatusCode, await res.Content.ReadAsStringAsync());
+        }
+
+        var stream = await res.Content.ReadAsStreamAsync();
+        var contentType = res.Content.Headers.ContentType?.ToString() ?? "application/octet-stream";
+
+        return new FileStreamResult(stream, contentType);
     }
 }
