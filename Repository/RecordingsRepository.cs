aef691616d592b021d1068522f55cfaf0d4291de
poproshaikin <stasstrong06@gmail.com>
2025-10-21 15:44:34 +0200

    fix mapping

--- Repository/RecordingsRepository.cs
+++ Repository/RecordingsRepository.cs
@@ -60,7 +60,7 @@ public class RecordingsRepository : RepositoryBase
                                           SELECT SUM(EXTRACT(EPOCH FROM (rp.end_date - rp.start_date)))
                                           FROM recording_parts rp
                                           WHERE rp.recording_id = r.id
-                                      ), 0)::DOUBLE PRECISION AS TotalSeconds
+                                      ), 0)::DOUBLE PRECISION AS "TotalSeconds"
                                FROM recordings r
                                WHERE r.user_id = @UserId;
                                """;
@@ -93,7 +93,7 @@ public class RecordingsRepository : RepositoryBase
                                SELECT SUM(EXTRACT(EPOCH FROM (rp.end_date - rp.start_date)))
                                FROM recording_parts rp
                                WHERE rp.recording_id = r.id
-                           ), 0)::DOUBLE PRECISION AS TotalSeconds
+                           ), 0)::DOUBLE PRECISION AS "TotalSeconds"
                     FROM recordings r
                     WHERE r.id = @Id;
                     """,
