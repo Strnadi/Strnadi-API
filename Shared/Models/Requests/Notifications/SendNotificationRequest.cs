d94c835450b081a5df6c4b85ca6b0d0d6f864bb9
poproshaikin <stasstrong06@gmail.com>
2025-11-03 11:37:05 +0100

    fix nullable types

--- Shared/Models/Requests/Notifications/SendNotificationRequest.cs
+++ Shared/Models/Requests/Notifications/SendNotificationRequest.cs
@@ -4,15 +4,15 @@ public class SendNotificationRequest
 {
     public int UserId { get; set; }
     
-    public string TitleEn { get; set; }
+    public string? TitleEn { get; set; }
     
-    public string BodyEn { get; set; }
+    public string? BodyEn { get; set; }
     
-    public string TitleDe { get; set; }
+    public string? TitleDe { get; set; }
     
-    public string BodyDe { get; set; }
+    public string? BodyDe { get; set; }
     
-    public string TitleCz { get; set; }
+    public string? TitleCz { get; set; }
     
-    public string BodyCz { get; set; }
+    public string? BodyCz { get; set; }
 }
