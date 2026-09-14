37933ab695dfe51355ada19b18102eeb446a3d4d
poproshaikin <stasstrong06@gmail.com>
2026-09-14 14:57:35 +0200

    add migration

---
+++ v2/src/Administration/Administration.Infrastructure/Persistence/Migrations/20260914125724_EncryptPostCode.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Administration.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EncryptPostCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "post_code",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "post_code",
+                table: "users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
